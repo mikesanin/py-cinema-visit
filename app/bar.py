6e26b1bbb396d719199967725420d082bf922b50
Misha Sanin <109426701+mikesanin@users.noreply.github.com>
2023-11-30 21:53:24 +0200

    Update bar.py

--- app/bar.py
+++ app/bar.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 
 class Customer:
-    pass
+    def __init__(self, name: str) -> None:
+        self.name = name
 
 
 class CinemaBar:
     @staticmethod
-    def sell_product(customers: Customer, product: str, customer=None) -> None:
+    def sell_product(customer: Customer, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
